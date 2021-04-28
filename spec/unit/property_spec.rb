21699c9959e7797cbfadd348d9127d92afa27b75
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-04-28 12:53:52 +0100

    Allowed the property object to take and return a user ID

--- spec/unit/property_spec.rb
+++ spec/unit/property_spec.rb
@@ -1,7 +1,12 @@
 require 'property'
 
 describe Property do
-  let(:property) {Property.new(id: 1, postcode: 'E19 4RH', title: 'Dummy property listing', description: 'generic property info', price_per_day: 100)}
+  let(:property) {Property.new(id: 1, postcode: 'E19 4RH', title: 'Dummy property listing', description: 'generic property info', user_id: "1", price_per_day: 100)}
+  
+  # let(:user_double) { double(:user, id: "1", name: "Finn", email: "finn@email.com") }
+  # let(:user_double_2) { double(:user, id: "2", name: "Matt", email: "matt@email.com") } 
+
+  #  *** couldn't get test doubles to work with databases - possible issues of doubles not existing in databases.
 
   context '#initialize' do
     it 'should have attributes' do
@@ -9,13 +14,22 @@ describe Property do
       expect(property.postcode).to eq 'E19 4RH'
       expect(property.title).to eq 'Dummy property listing'
       expect(property.description).to eq 'generic property info'
+      # expect(property.user_id).to eq 
       expect(property.price_per_day).to eq 100
+    end
+
+    it 'stores a user id' do
+      user = User.create('test name', "test@email.com", 'password1234')
+      property = Property.create(address: '123 fake street', postcode: 'E19 4RH', title: 'Dummy property listing', description: 'generic property info', user_id: user.id, price_per_day: 100)
+
+      expect(property.user_id).to eq user.id
     end
   end
 
   context '.create' do
     it 'should create a new instance of Property' do
-      property = Property.create(address: '123 fake street', postcode: 'E19 4RH', title: 'Dummy property listing', description: 'generic property info', price_per_day: 100)
+      user = User.create('test name', "test@email.com", 'password1234')
+      property = Property.create(address: '123 fake street', postcode: 'E19 4RH', title: 'Dummy property listing', description: 'generic property info', user_id: user.id, price_per_day: 100)
 
       expect(property.description).to eq 'generic property info'
       expect(property.postcode).to eq 'E19 4RH'
@@ -24,8 +38,10 @@ describe Property do
 
   context '.all' do
     it 'should return existing properties' do
-      property_1 = Property.create(address: '123 fake street', postcode: 'E19 4RH', title: 'Dummy property listing', description: 'generic property info', price_per_day: 100)
-      property_2 = Property.create(address: '124 fake street', postcode: 'E20 4RH', title: 'illusion property listing', description: 'misleading property info', price_per_day: 150)
+      user = User.create('test name', "test@email.com", 'password1234')
+      user_2 = User.create('test name 2', "test2@email.com", 'password12345')
+      property_1 = Property.create(address: '123 fake street', postcode: 'E19 4RH', title: 'Dummy property listing', description: 'generic property info', user_id: user.id, price_per_day: 100)
+      property_2 = Property.create(address: '124 fake street', postcode: 'E20 4RH', title: 'illusion property listing', description: 'misleading property info', user_id: user_2.id, price_per_day: 150)
 
       properties = Property.all
 
