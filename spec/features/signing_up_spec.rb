b26fce2145fde2349a09de9671a1d70be426937e
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-29 11:32:43 +0100

    password hidden then typing and checkbox to show password added

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -16,4 +16,14 @@ feature 'signing up' do
     sign_up
     expect(page).to have_content 'User already exists, please log in!'
   end
+
+  scenario 'User password is hidden' do
+    visit '/'
+    click_button 'Sign up'
+    fill_in('email', with: 'test@email.com')
+    fill_in('name', with: 'test name')
+    fill_in('password', with: 'test_password1')
+    expect(page).not_to have_content 'test_password1'
+  end
+  
 end
